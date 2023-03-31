7c0f2f762fc9bd9eaacd2b4a9cfa9ad79a1c77fc
govindansriram <sriramgovindanwork@gmail.com>
2023-03-30 21:08:13 -0700

    added one line command training

--- BenchKit/Data/Helpers.py
+++ BenchKit/Data/Helpers.py
@@ -1,4 +1,5 @@
 import os
+from colorama import Fore, Style
 from concurrent.futures import ThreadPoolExecutor
 import numpy as np
 import torch
@@ -12,7 +13,7 @@ from BenchKit.Data.Datasets import ProcessorDataset, ChunkDataset
 megabyte = 1_024 ** 2
 gigabyte = megabyte * 1024
 terabyte = gigabyte * 1024
-limit = 750 * megabyte
+limit = 100 * megabyte
 
 
 class UploadError(Exception):
@@ -24,7 +25,6 @@ def process_datasets(processed_dataset: ProcessorDataset,
                      dataset_name: str,
                      *args,
                      **kwargs):
-
     from BenchKit.Miscellaneous.BenchKit import set_settings, write_config
     from BenchKit.Miscellaneous.Settings import get_config, set_config
     set_settings()
@@ -40,6 +40,8 @@ def process_datasets(processed_dataset: ProcessorDataset,
             break
 
     if not skip:
+
+        print(Fore.RED + "Started Data processing" + Style.RESET_ALL)
         is_2 = False
         for i in DataLoader(processed_dataset, batch_size=1):
             if len(i) == 2:
@@ -51,29 +53,36 @@ def process_datasets(processed_dataset: ProcessorDataset,
         else:
             ds_list = save_label_data(processed_dataset, dataset_name)
 
+        affirm_size(ds_list[-1]["path"])
+
         set_config({"datasets": ds_list})
         cfg["datasets"] = ds_list
 
         write_config()
-        print("Data is processed")
+        print(Fore.GREEN + "Data is processed" + Style.RESET_ALL)
 
     dl = DataLoader(dataset=chunk_dataset(dataset_name, *args, **kwargs), num_workers=4, batch_size=16)
 
     if ds:
         if not ds.get("test"):
-            for _ in tqdm(dl):
+            print(Fore.RED + "Running Data Loading test" + Style.RESET_ALL)
+            for _ in tqdm(dl, colour="blue"):
                 pass
     else:
-        for _ in tqdm(dl):
+        for _ in tqdm(dl, colour="blue"):
             pass
 
     for idx, i in enumerate(cfg["datasets"]):
         if i["name"] == dataset_name:
             cfg["datasets"][idx]["test"] = True
 
+    ds = i
+
     if ds:
         set_config({"datasets": cfg["datasets"]})
         write_config()
+
+    print(Fore.GREEN + "Data Loading Test Passed" + Style.RESET_ALL)
 
 
 def copy_file(folder_path: str,
@@ -149,7 +158,8 @@ def save_file_and_label(dataset: ProcessorDataset,
 
     count = 0
 
-    for batch in tqdm(dataloader):
+    for batch in tqdm(dataloader,
+                      colour="blue"):
         count += 1
         labels, file = batch
 
@@ -213,7 +223,7 @@ def save_label_data(dataset: ProcessorDataset,
 
     folder_path = os.path.join(save_folder, folder_name.format(chunk_num))
 
-    for batch in tqdm(dataloader):
+    for batch in tqdm(dataloader, colour="blue"):
 
         if os.path.isdir(folder_path):
             arr.append(batch)
@@ -243,10 +253,86 @@ def save_label_data(dataset: ProcessorDataset,
     return ds_list
 
 
+def get_dir_size(path) -> int:
+    total = 0
+    with os.scandir(path) as it:
+        for entry in it:
+            if entry.is_file():
+                total += entry.stat().st_size
+            elif entry.is_dir():
+                total += get_dir_size(entry.path)
+    return total
+
+
+def merge_folders(small_folder: str, large_folder: str, save_folder: str):
+    small_path = os.path.join(save_folder, os.path.split(small_folder)[-1].split(".")[0])
+    large_path = os.path.join(save_folder, os.path.split(large_folder)[-1].split(".")[0])
+
+    shutil.unpack_archive(small_folder, small_path)
+    shutil.unpack_archive(large_folder, large_path)
+
+    small_tensor = []
+    large_tensor = []
+
+    small_files = []
+    large_files = []
+
+    lt_path = ''
+    lf_path = ''
+
+    for i in os.listdir(small_path):
+        tail = small_path.split(".")[0]
+        pth = os.path.join(tail, i)
+        if i.endswith(".pt"):
+            small_tensor: list = torch.load(pth)
+        else:
+            small_files = [os.path.join(pth, i) for i in os.listdir(pth)]
+
+    for i in os.listdir(large_path):
+        tail = large_path.split(".")[0]
+        pth = os.path.join(tail, i)
+        if i.endswith(".pt"):
+            lt_path = pth
+            large_tensor: list = torch.load(pth)
+        else:
+            lf_path = pth
+            large_files = [os.path.join(pth, i) for i in os.listdir(pth)]
+
+    large_tensor.extend(small_tensor)
+
+    if large_files:
+        large_files = sorted(large_files, key=lambda x: int(x.split("-")[-1]))
+        small_files = sorted(small_files, key=lambda x: int(x.split("-")[-1]))
+
+        last_int = int(large_files[-1].split("-")[-1])
+
+        with ThreadPoolExecutor(15) as exe:
+            _ = [exe.submit(shutil.copytree,
+                            i,
+                            os.path.join(lf_path, f"file-{idx + last_int}")) for idx, i in
+                 enumerate(small_files)]
+
+    torch.save(large_tensor, lt_path)
+    os.remove(small_folder)
+    os.remove(large_folder)
+    shutil.rmtree(small_path)
+
+    shutil.make_archive(large_folder,
+                        "zip",
+                        large_path)
+
+    shutil.rmtree(large_path)
+
+    return large_folder
+
+
 # test in the morning
 def affirm_size(save_folder: str):
     pass_size_requirement = []
     fails_size_requirement = []
+
+    if get_dir_size(save_folder) < megabyte * 100:
+        raise RuntimeError("Dataset must be greater than 300 megabytes")
 
     for i in os.listdir(save_folder):
         path: str = os.path.join(save_folder, i)
@@ -256,68 +342,26 @@ def affirm_size(save_folder: str):
         else:
             fails_size_requirement += [path]
 
-    while len(fails_size_requirement) > 0:
+    while len(fails_size_requirement) != 1:
+        large_folder = fails_size_requirement[0]
         small_folder = fails_size_requirement.pop()
-        large_folder = pass_size_requirement.pop()
 
-        small_path = os.path.join(save_folder, os.path.split(small_folder)[-1].split(".")[-1])
-        large_path = os.path.join(save_folder, os.path.split(large_folder)[-1].split(".")[-1])
+        large_folder = merge_folders(small_folder, large_folder, save_folder)
 
-        shutil.unpack_archive(small_folder, small_path)
-        shutil.unpack_archive(large_folder, large_path)
+        size = os.path.getsize(large_folder)
 
-        small_tensor = []
-        large_tensor = []
+        if size >= megabyte * 100:
+            pass_size_requirement.append(fails_size_requirement.pop(0))
 
-        small_files = []
-        large_files = []
+    if len(pass_size_requirement) > 0:
 
-        lt_path = ''
-        lf_path = ''
+        while len(fails_size_requirement) > 0:
+            small_folder = fails_size_requirement.pop()
+            large_folder = pass_size_requirement.pop()
 
-        for i in os.listdir(small_path):
-            pth = os.path.join(small_folder, i)
-            if i.endswith(".pt"):
-                small_tensor: list = torch.load(pth)
-            else:
-                small_files = [os.path.join(pth, i) for i in os.listdir(pth)]
+            large_folder = merge_folders(small_folder, large_folder, save_folder)
 
-        for i in os.listdir(large_path):
-            pth = os.path.join(large_folder, i)
-            if i.endswith(".pt"):
-                lt_path = pth
-                large_tensor: list = torch.load(pth)
-            else:
-                lf_path = pth
-                large_files = [os.path.join(pth, i) for i in os.listdir(pth)]
-
-        large_tensor.extend(small_tensor)
-
-        if large_files:
-            large_files = sorted(large_files, key=lambda x: int(x.split("-")[-1]))
-            small_files = sorted(small_files, key=lambda x: int(x.split("-")[-1]))
-
-            last_int = int(large_files[-1].split("-")[-1])
-
-            with ThreadPoolExecutor(15) as exe:
-                _ = [exe.submit(shutil.copyfile,
-                                i,
-                                os.path.join(os.path.split(lf_path)[0],
-                                             f"file-{idx + last_int}")) for idx, i in enumerate(small_files)]
-
-        torch.save(large_tensor, lt_path)
-
-        os.remove(small_folder)
-        os.remove(large_folder)
-        shutil.rmtree(small_path)
-
-        shutil.make_archive(large_folder,
-                            "zip",
-                            large_path)
-
-        shutil.rmtree(large_path)
-
-        pass_size_requirement.insert(0, large_folder)
+            pass_size_requirement.insert(0, large_folder)
 
 
 def iterate_directory(file_dir: str) -> tuple[str, bool]:
