5604ebf0a106d3e4ef40092ce3784db44a307af9
govindansriram <sriramgovindanwork@gmail.com>
2023-04-14 14:20:28 -0700

    made dataloading 40% faster

--- BenchKit/Data/Datasets.py
+++ BenchKit/Data/Datasets.py
@@ -1,8 +1,5 @@
-import io
 import json
 import os
-import zipfile
-
 import requests
 import torch
 from torch.utils.data import Dataset
@@ -12,6 +9,8 @@ import multiprocessing
 
 from BenchKit.Miscellaneous.Settings import get_config
 from BenchKit.Miscellaneous.User import get_dataset, get_get_url
+
+lock = multiprocessing.Lock()
 
 
 class ProcessorDataset(Dataset):
@@ -30,13 +29,44 @@ class ProcessorDataset(Dataset):
         return (cur_num, cur_file) if cur_file else (cur_num,)
 
 
+# Add the ability to use accelerate
+# Change code so it works using multiple gpu's
+# -- This means that locks and events have to be gpu specific
+# -- make a new lock class
+# Also you need to change it so that the dataset size is in the zip name
+# -- This allows any process to continue from any dataset slice
+# after this make an accelerated model and see if this works
+
 class ChunkDataset(Dataset):
 
+    @staticmethod
+    def update_process_count(process_name: str | None = None) -> int:
+        with open("Process.json", "r") as j:
+            process_dict = json.load(j)
+
+            if process_name:
+                process_dict["process_list"].append(process_name)
+
+        with open("Process.json", "w") as j:
+            json.dump(process_dict, j)
+
+        # lock.release()
+        return len(process_dict["process_list"])
+
+    @staticmethod
+    def new_process_json():
+        with open("Process.json", "w") as file:
+            json.dump({"process_list": []}, file)
+
+    # here add gpu count
+    # add num workers per gpu this number should depend on the gpu the user plans to use
     def __init__(self,
                  name: str,
                  cloud: bool):
 
         cfg = get_config()
+
+        ChunkDataset.new_process_json()
 
         for i in cfg.get("datasets"):
             if i["name"] == name:
@@ -53,6 +83,7 @@ class ChunkDataset(Dataset):
 
         if not cloud:
             self._chunk_list = [os.path.join(chunk_path, chunk) for chunk in os.listdir(chunk_path)]
+            print(self._chunk_list)
         else:
             self._chunk_list = get_dataset(dataset_id)
 
@@ -60,36 +91,33 @@ class ChunkDataset(Dataset):
 
         self._pos = len(self._label_chunk)
         self._prev_doc_len = 0
+        self._write_file_event = multiprocessing.Event()
 
-    def get_current_labels_and_files(self):
+    def unzip_labels_and_files(self,
+                               current_chunk: str,
+                               chunk_path: str):
 
-        process = multiprocessing.current_process().name
-
-        root_dir = os.path.join(".", f"TempData-{process}")
+        root_dir = os.path.join(".", f"TempData")
 
         if os.path.isdir(root_dir):
             shutil.rmtree(root_dir)
 
         os.mkdir(root_dir)
 
-        current_chunk = self._chunk_list.pop(0)
-
         if not self._cloud:
-            self._chunk_path = os.path.join(root_dir,
-                                            os.path.split(current_chunk)[-1])
 
             try:
-                shutil.unpack_archive(current_chunk, self._chunk_path)
+                shutil.unpack_archive(current_chunk, chunk_path)
             except FileExistsError:
                 pass
         else:
 
             file_data = get_get_url(dataset_id=self._dataset_id,
-                                               file_path=current_chunk)
+                                    file_path=current_chunk)
 
             mem_zip = requests.get(file_data)
 
-            zip_dir = os.path.join(".", f"TempData-zip-{process}")
+            zip_dir = os.path.join(".", "TempData-zip")
             if os.path.isdir(zip_dir):
                 shutil.rmtree(zip_dir)
 
@@ -99,14 +127,20 @@ class ChunkDataset(Dataset):
             with open(zip_path, 'wb') as f:
                 f.write(mem_zip.content)
 
-            self._chunk_path = os.path.join(root_dir,
-                                            os.path.split(current_chunk)[-1])
-
             try:
-                shutil.unpack_archive(zip_path, self._chunk_path)
+                shutil.unpack_archive(zip_path, chunk_path)
             except FileExistsError:
                 pass
 
+    def set_new_chunk_path(self):
+        root_dir = os.path.join(".", f"TempData")
+        current_chunk = self._chunk_list.pop(0)
+        self._chunk_path = os.path.join(root_dir,
+                                        os.path.split(current_chunk)[-1])
+
+        return current_chunk, self._chunk_path
+
+    def _set_new_data(self):
         folder_list = os.listdir(self._chunk_path)
 
         for i in folder_list:
@@ -131,9 +165,24 @@ class ChunkDataset(Dataset):
             return None
 
     def __getitem__(self, idx):
+
         if idx >= self._pos:
+            lock.acquire()
+            p_len = ChunkDataset.update_process_count(str(os.getpid()))
             self._prev_doc_len += len(self._label_chunk)
-            self.get_current_labels_and_files()
+            c1, c2 = self.set_new_chunk_path()
+
+            if p_len == 4:
+                self.unzip_labels_and_files(c1, c2)
+                ChunkDataset.new_process_json()
+                self._write_file_event.set()
+                lock.release()
+                self._write_file_event.clear()
+            else:
+                lock.release()
+                self._write_file_event.wait()
+
+            self._set_new_data()
             self._pos += len(self._label_chunk)
 
         file_tup = self.get_files(idx - self._prev_doc_len)
