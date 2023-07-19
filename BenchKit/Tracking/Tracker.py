64bf212d68b92595ea3e571b57c23cc698040007
govindansriram <sriramgovindanwork@gmail.com>
2023-07-19 11:07:28 -0400

    Added tensorboard plugins

--- BenchKit/Tracking/Tracker.py
+++ BenchKit/Tracking/Tracker.py
@@ -1,9 +1,68 @@
+import copy
 import json
 from accelerate.tracking import GeneralTracker, on_main_process
 import os
 import pandas as pd
-
 from BenchKit.Miscellaneous.User import update_server
+from accelerate import Accelerator
+from accelerate.utils import FullyShardedDataParallelPlugin, ProjectConfiguration, MegatronLMPlugin, \
+    GradientAccumulationPlugin, DeepSpeedPlugin
+
+
+class AcceleratorInitializer:
+
+    @staticmethod
+    def accelerator(split_batches=False,
+                    log_with=None,
+                    even_batches: bool = True,
+                    step_scheduler_with_optimizer: bool = True,
+                    dynamo_backend: str = "no",
+                    gradient_accumulation_steps: int = 1,
+                    fsdp_plugin: FullyShardedDataParallelPlugin = None,
+                    deepspeed_plugin: DeepSpeedPlugin = None,
+                    megatron_lm_plugin: MegatronLMPlugin = None,
+                    project_config: ProjectConfiguration | None = None,
+                    gradient_accumulation_plugin: GradientAccumulationPlugin | None = None,
+                    dispatch_batches: bool | None = None,
+                    mixed_precision: str | None = None,
+                    rng_types: list[str] | None = None,
+                    kwargs_handlers: list | None = None) -> Accelerator:
+
+        kwargs = {'split_batches': split_batches,
+                  'log_with': log_with,
+                  'even_batches': even_batches,
+                  'step_scheduler_with_optimizer': step_scheduler_with_optimizer,
+                  'dynamo_backend': dynamo_backend,
+                  'gradient_accumulation_steps': gradient_accumulation_steps,
+                  'mixed_precision': mixed_precision,
+                  'rng_types': rng_types,
+                  'kwargs_handlers': kwargs_handlers,
+                  'fsdp_plugin': fsdp_plugin,
+                  'deepspeed_plugin': deepspeed_plugin,
+                  'megatron_lm_plugin': megatron_lm_plugin,
+                  'project_config': project_config,
+                  'gradient_accumulation_plugin': gradient_accumulation_plugin,
+                  'dispatch_batches': dispatch_batches}
+
+        for k, v in copy.deepcopy(kwargs).items():
+
+            if not v:
+                kwargs.pop(k)
+
+        return Accelerator(**kwargs)
+
+
+def get_tensorboard_tracker(config: dict[str: str],
+                            *args,
+                            **kwargs):
+
+    kwargs["project_config"] = ProjectConfiguration(project_dir=".", logging_dir=os.getenv("LOG_DIR"))
+    kwargs["log_with"] = "tensorboard"
+
+    acc = AcceleratorInitializer.accelerator(**kwargs)
+    acc.init_trackers(os.getenv("EXPERIMENT_NAME"), config=config)
+
+    return acc, *acc.prepare(*args)
 
 
 class BenchAccelerateTracker(GeneralTracker):
@@ -112,9 +171,3 @@ class ScatterPlot(BenchTracker):
               **kwargs):
         self._pd.iloc[0] = pd.Series(data={"line_name": dataset_name, self._axis_one: x, **kwargs})
         super().write()
-
-
-if __name__ == '__main__':
-    sc = ScatterPlot("my_x", ["loss", "correct_class"], "test", {"lr": 1})
-    sc.write("train", 1, loss=10, correct_class_ratio=100)
-    sc.write("train", 3, loss=None, correct_class_ratio=1)
--- BenchKit/Train/Helpers.py
+++ BenchKit/Train/Helpers.py
@@ -1,42 +1,22 @@
-import copy
 import os.path
 from pathlib import Path
-
 from accelerate import Accelerator
+from BenchKit.Data.Helpers import remove_all_temps
 
 
-def get_accelerator(split_batches=False,
-                    log_with=None,
-                    even_batches: bool = True,
-                    step_scheduler_with_optimizer: bool = True,
-                    dynamo_backend: str = "no",
-                    gradient_accumulation_steps: int = 1,
-                    mixed_precision: str | None = None,
-                    cpu: bool | None = None,
-                    rng_types: list[str] | None = None,
-                    kwargs_handlers: list | None = None) -> Accelerator:
+def data_loading(acc: Accelerator):
+    def decorator(fun):
+        def wrapper(*args, **kwargs):
+            return fun(*args, **kwargs)
+        wipe_temp(acc)
+        acc.free_memory()
 
-    kwargs = {'split_batches': split_batches,
-              'log_with': log_with,
-              'even_batches': even_batches,
-              'step_scheduler_with_optimizer': step_scheduler_with_optimizer,
-              'dynamo_backend': dynamo_backend,
-              'gradient_accumulation_steps': gradient_accumulation_steps,
-              'mixed_precision': mixed_precision,
-              'cpu': cpu,
-              'rng_types': rng_types,
-              'kwargs_handlers': kwargs_handlers}
+        return wrapper
 
-    for k, v in copy.deepcopy(kwargs).items():
-
-        if not v:
-            kwargs.pop(k)
-
-    return Accelerator(**kwargs)
+    return decorator
 
 
 def wipe_temp(acc: Accelerator):
-    from BenchKit.Data.Helpers import remove_all_temps
     acc.wait_for_everyone()
     remove_all_temps()
     acc.wait_for_everyone()
