c9e65b1203468806539858c86b1d55398cb8a6ba
govindansriram <sriramgovindanwork@gmail.com>
2023-08-14 13:51:16 -0400

    changed readme

--- BenchKit/Data/Helpers.py
+++ BenchKit/Data/Helpers.py
@@ -56,6 +56,7 @@ def get_dataset(chunk_class,
                 num_workers: int,
                 *args,
                 **kwargs):
+
     chunk_dataset: IterableChunk = chunk_class(*args, **kwargs)
 
     chunk_dataset.post_init(dataset_name,
