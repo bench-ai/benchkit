95583f75a619b56314153ee4039d02bd451309d9
govindansriram <sriramgovindanwork@gmail.com>
2023-08-27 20:03:34 -0400

    added comments to Config class

--- BenchKit/tracking/config.py
+++ BenchKit/tracking/config.py
@@ -1,10 +1,8 @@
 import os
 from pathlib import Path
-
+from BenchKit.tracking.save import upload_model_state, upload_model_save
 from BenchKit.Miscellaneous.requests.server import init_config
 from typing import Callable
-
-from BenchKit.tracking.save import upload_model_state, upload_model_save
 
 
 class Config:
@@ -46,6 +44,13 @@ class Config:
         config_folder_name = f"bench-{self.config_id}-config"
 
         self.config_save_path = Path(__file__).resolve().parent.parent / root_folder_name / config_folder_name
+        self.create_checkpoint_and_save_directory()
+
+    def create_checkpoint_and_save_directory(self) -> None:
+        """
+        Creates the directory where the config checkpoints will be saved, and the directory where the model saves will
+        be saved.
+        """
 
         os.makedirs(os.path.join(str(self.config_save_path), "checkpoints"),
                     exist_ok=True)
@@ -54,23 +59,48 @@ class Config:
                     exist_ok=True)
 
     @property
-    def last_iteration(self):
+    def last_iteration(self) -> int:
+        """
+        returns the last iteration that was saved
+        """
         return self._last_iteration
 
     @last_iteration.setter
-    def last_iteration(self, iteration: int):
+    def last_iteration(self, iteration: int) -> None:
+        """
+        Checks if the current iteration is greater than the last iteration and sets the current iteration to the last
+        iteration
+
+        :param iteration:
+        """
         if self.last_iteration < iteration:
             raise ValueError(f"provided iteration: {iteration}, is less than last iteration {self.last_iteration}")
         else:
             self._last_iteration = iteration
 
     def should_save(self, eval_value: float) -> bool:
+        """
+        Models should only be saved when they outperform the previous save. This function checks if the evaluation
+        metric beats the previous best evaluation metric.
+
+        :param eval_value:
+        :return: a boolean dictating if this model should be saved
+        """
         return (self.mode == "max") == (eval_value > self.best_evaluation_value)
 
     def save_and_upload_state(self,
                               save_func: Callable[..., str],
                               iteration: int,
-                              evaluation_value: float):
+                              evaluation_value: float) -> None:
+
+        """
+        Saves the model's state and uploads it to the bench bucket
+
+        :param save_func: a function that saves the state of your model into a directory should return a string which
+        is the path to that directory
+        :param iteration:
+        :param evaluation_value:
+        """
 
         state_dir_path: str = save_func()
 
@@ -85,7 +115,15 @@ class Config:
 
     def save_and_upload_save(self,
                              save_func: Callable[..., str],
-                             evaluation_value: float):
+                             evaluation_value: float) -> None:
+
+        """
+        Saves the model and uploads it to the bench bucket
+
+        :param save_func: a function that saves your model into a directory should return a string which
+        is the path to that directory
+        :param evaluation_value:
+        """
 
         save_dir_path: str = save_func()
 
@@ -97,11 +135,23 @@ class Config:
                           evaluation_value,
                           self)
 
+        self.best_evaluation_value = evaluation_value
+
     def save_model_and_state(self,
                              save_state_func: Callable[..., str],
                              save_model_func: Callable[..., str],
                              iteration: int,
-                             evaluation_value: float):
+                             evaluation_value: float) -> None:
+
+        """
+        Saves the state of your model, and saves the model if the evaluation value is better than the previous
+        evaluation value
+
+        :param save_state_func: function that saves your model's state to a directory returns the path to the dir
+        :param save_model_func: function that saves your model to a dir, returns the path to that dir
+        :param iteration:
+        :param evaluation_value:
+        """
 
         self.last_iteration = iteration
         self.save_and_upload_state(save_state_func,
@@ -111,4 +161,3 @@ class Config:
         if self.should_save(evaluation_value):
             self.save_and_upload_save(save_model_func,
                                       evaluation_value)
-
