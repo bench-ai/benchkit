b37c2f304ac4e5616c38343debecd132f8a2f1f3
govindansriram <sriramgovindanwork@gmail.com>
2023-09-03 19:04:02 -0700

    readded generate tar method

--- BenchKit/Miscellaneous/utils/tar.py
+++ BenchKit/Miscellaneous/utils/tar.py
@@ -27,6 +27,13 @@ def tar_gzip_folder(tar_name: str,
     return tar_save_path
 
 
+def generate_tar(tar_name: str, file_path: str):
+    with tarfile.open(f"{tar_name}.tar.gz", "w:gz") as tar:
+        tar.add(file_path, arcname=os.path.split(file_path)[-1])
+
+    return f"{tar_name}.tar.gz"
+
+
 def extract_tar(tar_path: str, extraction_path: str):
     file = tarfile.open(tar_path)
     file.extractall(extraction_path)
